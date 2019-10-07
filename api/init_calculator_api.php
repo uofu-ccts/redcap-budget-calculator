70c7bfcc64887fe1968e3a4b7f707f20b8bb208d
Terrance Davis <tdavis@tdavis-mac.local>
2019-10-07 16:19:00 -0600

    more api conversions

---
+++ api/init_calculator_api.php
@@ -0,0 +1,23 @@
+<?php
+/** @var \UIOWA\BudgetCalculator\BudgetCalculator $module */
+
+// Uncomment following lines to add validation
+// if (! $module->verifyApiCaller())
+// {
+//   echo('{"error":"authentication failure"}');
+//   return;
+// }
+
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+//setting up array of records for converting to JSON
+$response_arr = $module->getBudgetCalculatorData();
+
+
+//Response code 200 ... OK
+http_response_code(200);
+
+//JSON response
+echo json_encode($response_arr);
